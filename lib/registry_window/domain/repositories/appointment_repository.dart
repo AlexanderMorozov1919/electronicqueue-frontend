1ad36a5ac20bc120ffb67946e8b4f37620f2b226
IlyushinDM <ilyushin_dm@outlook.com>
2025-07-25 02:16:18 +0300

    feat: Добавлена форма просмотра записей у регистратора

--- lib/registry_window/domain/repositories/appointment_repository.dart
+++ lib/registry_window/domain/repositories/appointment_repository.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import '../../core/errors/failures.dart';
 import '../entities/doctor_entity.dart';
 import '../entities/schedule_slot_entity.dart';
+import '../entities/appointment_details_entity.dart';
 
 abstract class AppointmentRepository {
   Future<Either<Failure, List<DoctorEntity>>> getActiveDoctors();
@@ -11,4 +12,7 @@ abstract class AppointmentRepository {
     required int patientId,
     required int ticketId,
   });
+  Future<Either<Failure, List<AppointmentDetailsEntity>>> getPatientAppointments(int patientId);
+  Future<Either<Failure, void>> deleteAppointment(int appointmentId);
+  Future<Either<Failure, void>> confirmAppointment({required int appointmentId, required int ticketId});
 }
